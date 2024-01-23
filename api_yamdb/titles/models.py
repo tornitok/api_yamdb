d374d8b2ee0613d81b30646c5bf9c5a0cbf23363
Владимир Бондаренко <AK23041989@live.com>
2024-01-23 10:35:48 +0600

    Добавить модели: Titles, Categories и Genres, а так же пакет-django_filters для поиска и фильтрации API

---
+++ api_yamdb/titles/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class CategoriesModel(models.Model):
+    """Категории (типы) произведений."""
+
+    name = models.CharField('Наименование категории', max_length=256)
+    slug = models.SlugField('Slug', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class GenresModel(models.Model):
+    """Категории жанров."""
+
+    name = models.CharField('Наименование жанра', max_length=256)
+    slug = models.SlugField('Slug', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class TitlesModel(models.Model):
+    """Произведения, к которым пишут отзывы
+    (определённый фильм, книга или песенка)."""
+
+    name = models.TextField('Наименование произведения', max_length=256)
+    category = models.OneToOneField(
+        CategoriesModel,
+        verbose_name='Категория произведения',
+        on_delete=models.RESTRICT,
+    )
+    genres = models.ManyToManyField(
+        GenresModel, verbose_name='Жанры произведения'
+    )
+    year = models.IntegerField('Год создания произведения')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
